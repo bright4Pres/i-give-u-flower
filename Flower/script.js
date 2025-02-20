2ee55e68e26ba07b23f1dda5e355cc1d0a973100
Bright Bastasa <knowncarbon@gmail.com>
2025-02-20 21:30:19 +0800

    ticket system

--- Flower/script.js
+++ Flower/script.js
@@ -1,3 +1,19 @@
-onload = () =>{
-        document.body.classList.remove("container");
+onload = () => {
+  document.body.classList.remove("container");
 };
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('button');
+  button.addEventListener('click', () => {
+    const inputs = document.querySelectorAll('.black_card .input');
+    let inputValue = '';
+    inputs.forEach(input => {
+      inputValue += input.value;
+    });
+    if (inputValue === '011125') {
+      window.location.href = './message/index.html';
+    } else {
+      alert('wrong password, try again');
+    }
+  });
+});
